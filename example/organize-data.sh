bb07e86c49916679cf6f6c87b50bc95d21f4daf4
mprorock <mprorock@mesur.io>
2022-02-13 16:58:34 -0500

    chore: clean up some error handling to prevent confusion

--- example/organize-data.sh
+++ example/organize-data.sh
@@ -1,3 +1,3 @@
-mv ./child-ca/1.* ./child-ca/1.example.com/
-mv ./child-ca/2.* ./child-ca/2.example.com/
-mv ./child-ca/3.* ./child-ca/3.example.com/
+mv ./child-ca/1.* ./child-ca/1.example.com/ > /dev/null 2>&1
+mv ./child-ca/2.* ./child-ca/2.example.com/ > /dev/null 2>&1
+mv ./child-ca/3.* ./child-ca/3.example.com/ > /dev/null 2>&1
